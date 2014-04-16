0d10993bb87fe5ac298793fed474e3ee621b2616
adamhsparks <adam.h.sparks@gmail.com>
2014-04-16 17:14:59 +0800

    Ecocrop CRU CL2.0
    Further refinement and development of the script. Addition of dismo
    library and now download and import CRU data files within R.

--- Models/Ecocrop CRU CL2.0 Potato Growing Seasons.R
+++ Models/Ecocrop CRU CL2.0 Potato Growing Seasons.R
@@ -8,16 +8,26 @@
 # remarks 1     : Download the CRU CL 2.0 Data here: http://www.cru.uea.ac.uk/cru/data/hrg/timm/grid/CRU_TS_2_0.html;
 ##############################################################################
 
-
-
 library(raster)
+library(dismo)
 
-wrld <- raster(nrows = 900, ncols = 2160)
+# Download and read CRU data files
+url.dtr <- "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_dtr.dat.gz" # mean diurnal temperature range
+file.dtr <- "data/CRU_dtr.gz"
+download.file(url.dtr, destfile = file.dtr, mode = "wb")
+dtr <- read.table('data/CRU_dtr.gz')
 
 col.names <- c('lat', 'lon', 'jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec')
-dtr <- read.table('Data/CRU CL2.0/grid_10min_dtr.dat')
 names(dtr) <- col.names
+
+
+
+# Set up a raster object to use as the basis for converting CRU data to raster objects
+wrld <- raster(nrows = 900, ncols = 2160)
+
 
 points <- dtr[, c(2, 1)]
 jan.tmin <- rasterize(x = points, y = wrld, field = dtr[, 3], fun = mean)
 
+
+#eos
