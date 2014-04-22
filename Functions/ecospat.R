e27879eac2d3610f0650078f0488033bac651d8c
adamhsparks <adam.h.sparks@gmail.com>
2014-04-22 17:27:38 +0800

    Updates and added functionality
    Updates to ecospat.R to bring into line with new version of ECOCROP in
    dismo. ECOCROP CRU CL2.0 Potato Growing Seasons.R now has code to
    download MIRCA harvest area as a global mask for potato areas.

--- Functions/ecospat.R
+++ Functions/ecospat.R
@@ -8,54 +8,37 @@
 # remarks 1     : ;
 ##############################################################################
 
-ecospat <- function(crop, tmn, tmx, tmp, pre, rainfed = TRUE, filename = '', ...) {
-  pot <- ecocropCrop(crop)
-  outr <- raster(tmp)
-  filename <- trim(filename)
-  v <- vector(length=ncol(outr))
-  if (filename == '') {
-    vv <- matrix(ncol = nrow(outr), nrow = ncol(outr))
-  }
+ecospat <- function(crop, tmn, tmx, tmp, pre, rainfed = TRUE, filename, ...) {
+  pot       <- getCrop(crop)
+  filename  <- trim(filename)
+  outr      <- raster(tmp)
+  v         <- vector(length = ncol(outr))
   for (r in 1:nrow(outr)){
     v[] <- NA
-    
     
     temp <- getValues(tmp, r) / 10
     tmin <- getValues(tmn, r) / 10
     if (rainfed) { prec <- getValues(pre, r) / 10 }
-    nac <- which(!is.na(tmin[,1]))
-    for (c in nac) {
+    
+    nac <- which(!is.na(tmin[, 1]))
+    
+    for (i in nac) {
       if (rainfed) {
-        clm <- cbind(data.frame(tmin[c,]), temp[c,],  prec[c,])
+        clm <- cbind(data.frame(tmin[i,]), temp[i,],  prec[i,])
       } else {
-        clm <- cbind(data.frame(tmin[c,]), temp[c,])
+        clm <- cbind(data.frame(tmin[i, ]), temp[i, ])
       }
+      
       if(sum(is.na(clm)) == 0) {
-        e <- ecocrop(clm, pot, rain=rainfed)
-        v[c] <- e@maxper[1]
+        e <- ecocrop(crop = 'potato', clm[, 1], clm[, 2], clm[, 3], rain = rainfed) 
+        v[i] <- e@maxper[1]
       }
     }
-    if (filename == '') {
-      vv[,r] <- v
-    } else {
-      outr <- setValues(outr, v, r)
-      outr <- writeRaster(outr, filename, ...)
-    }
+    
+    outr[r, ] <- v
+    outr <- writeRaster(outr, filename, ...)
   }
-  if (filename == '') { outr <- setValues(outr, as.vector(vv))  }
   return(outr)
 }
-
-
-#alt  <- raster('G:/data/grids/cru/New10m/alt', values=TRUE)
-tmp  <- stack(paste('G:/data/grids/cru/New10m/tmp', 1:12, sep = ''))
-tmn  <- stack(paste('G:/data/grids/cru/New10m/tmn', 1:12, sep = ''))
-tmx  <- stack(paste('G:/data/grids/cru/New10m/tmx', 1:12, sep = ''))
-pre  <- stack(paste('G:/data/grids/cru/New10m/pre', 1:12, sep = ''))
-# to speed things up a bit:
-tmp <- readAll(tmp)
-tmn <- readAll(tmn)
-tmx <- readAll(tmx)
-pre <- readAll(pre)
 
 #eos
--- Models/Ecocrop CRU CL2.0 Potato Growing Seasons.R
+++ Models/Ecocrop CRU CL2.0 Potato Growing Seasons.R
@@ -22,7 +22,7 @@ source ('../Functions/ecospat.R')
 url.dtr <- "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_dtr.dat.gz" 
 file.dtr <- "Data/CRU_dtr.gz"
 download.file(url.dtr, destfile = file.dtr, mode = "wb")
-dtr <- read.table('data/CRU_dtr.gz')
+dtr <- read.table('Data/CRU_dtr.gz')
 xy <- dtr[, c(2, 1)]
 dtr <- dtr[, c(-1, -2)]
 
@@ -30,14 +30,14 @@ dtr <- dtr[, c(-1, -2)]
 url.tmp <- "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_tmp.dat.gz" 
 file.tmp <- "Data/CRU_tmp.gz"
 download.file(url.tmp, destfile = file.tmp, mode = "wb")
-tmp <- read.table('data/CRU_tmp.gz')
+tmp <- read.table('Data/CRU_tmp.gz')
 tmp <- tmp[, c(-1, -2)]
 
 ## mean monthly precipitation #####
 url.pre <- "http://www.cru.uea.ac.uk/cru/data/hrg/tmc/grid_10min_pre.dat.gz" 
 file.pre <- "Data/CRU_pre.gz"
 download.file(url.pre, destfile = file.pre, mode = "wb")
-pre <- read.table('data/CRU_pre.gz')
+pre <- read.table('Data/CRU_pre.gz')
 pre <- pre[, c(-1, -2)]
 
 ##### column names and later layer names for raster stack objects ####
@@ -70,7 +70,41 @@ tmx.stack <- create.stack(tmx, xy, wrld)
 
 #### run ECOCROP model on raster stack of precipitation, tmin and tmax #####
 
-r <- ecospat('Potato', tmn, tmx, tmp, pre, rainfed = TRUE,
-             filename = 'potplant.grd')
+pot       <- getCrop('potato')
+pot@RMIN  <- 125
+pot@ROPMN <- 250
+pot@ROPMX <- 350
+pot@TMIN  <- 7
+pot@TOPMX <- 20
+pot@GMIN  <- pot@GMAX <- 100
+
+prf <- ecospat('potato', tmn, tmx, tmp, pre, rainfed = TRUE, filename = 'Cache/Planting Seasons/CRUCL2.0_PRF.grd', overwrite = TRUE) # Rainfed potato
+pir <- ecospat('potato', tmn, tmx, tmp, pre, rainfed = FALSE, filename = 'Cache/Planting Seasons/CRUCL2.0_PIR.grd', overwrite = TRUE) # Irrigated potato
+
+#### Download MIRCA 2000 Maximum Harvested Area for Potato (Crop #10) to use as a mask ####
+url.pot <- "ftp://ftp.rz.uni-frankfurt.de/pub/uni-frankfurt/physische_geographie/hydrologie/public/data/MIRCA2000/harvested_area_grids/ANNUAL_AREA_HARVESTED_RFC_CROP10_HA.ASC.gz"
+file.pot <- "Data/ANNUAL_AREA_HARVESTED_RFC_CROP10_HA.ASC.gz"
+download.file(url.pot, destfile = file.pot, mode = "wb")
+untar("Data/ANNUAL_AREA_HARVESTED_RFC_CROP10_HA.ASC.gz")
+pot <- raster("Data/ANNUAL_AREA_HARVESTED_RFC_CROP10_HA.ASC.gz")
+xy <- dtr[, c(2, 1)]
+dtr <- dtr[, c(-1, -2)]
+
+
+rfp <- raster(paste('tmp/poplant_a2', gcm, '_', timeslice, '_PRF.grd', sep = '')) # rainfed potato
+rfp <- reclassify(rfp, c(0, 0, NA), include.lowest = TRUE) # set values of 0 equal to NA
+
+irp <- raster(paste('tmp/poplant_a2', gcm, '_', timeslice, '_PIR.grd', sep = '')) # irrigated potato
+irp <- reclassify(irp, c(0, 0, NA), include.lowest = TRUE) # set values of 0 equal to NA
+
+comb <- cover(rfp, irp)
+comb <- reclassify(comb, c(0, 0, NA), include.lowest = TRUE)
+
+poplant.mask <- raster('data/MIRCA_Poplant.grd') # Mask to remove non-potato areas
+poplant <- mask(comb, poplant.mask) # mask the non-potato growing areas from raster
+
+writeRaster(poplant, filename = paste('Cache/, '.grd', sep = ''), overwrite = TRUE)
+
+# eos
 
 #eos
