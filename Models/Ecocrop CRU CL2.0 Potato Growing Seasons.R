7b7bdae3c769c9094c9ca3ad230b3ed0e2323668
adamhsparks <adam.h.sparks@gmail.com>
2014-04-30 10:22:53 +0800

    Update header info with licence information

--- Models/Ecocrop CRU CL2.0 Potato Growing Seasons.R
+++ Models/Ecocrop CRU CL2.0 Potato Growing Seasons.R
@@ -4,8 +4,21 @@
 # producer      : prepared by R. Hijmans and A. Sparks;
 # last update   : in Los Baños, Laguna, April 2014;
 # inputs        : CRU CL2.0 Climate data;
-# outputs       : PotatoPlant_CRUCL2.0.grd;
+# outputs       : PotatoPlant_CRUCL2.0.grd, CRUCL2.0_PRF.grd, CRUCL2.0_PIR.grd;
 # remarks 1     : Download the CRU CL 2.0 Data here: http://www.cru.uea.ac.uk/cru/data/hrg/timm/grid/CRU_TS_2_0.html;
+# Licence:      : This program is free software; you can redistribute it and/or modify
+#                 it under the terms of the GNU General Public License as published by
+#                 the Free Software Foundation; either version 2 of the License, or
+#                 (at your option) any later version.
+
+#                 This program is distributed in the hope that it will be useful,
+#                 but WITHOUT ANY WARRANTY; without even the implied warranty of
+#                 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#                 GNU General Public License for more details.
+
+#                 You should have received a copy of the GNU General Public License along
+#                 with this program; if not, write to the Free Software Foundation, Inc.,
+#                 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ##############################################################################
 
 #### Libraries ####
