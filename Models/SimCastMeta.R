b2e7900b21f2a4bd8b13eab243794568fbf5c899
Adam H. Sparks <adamhsparks@gmail.com>
2014-05-09 20:15:06 +0800

    License updates in scripts

--- Models/SimCastMeta.R
+++ Models/SimCastMeta.R
@@ -10,19 +10,7 @@
 #                 Sparks, A. H., Forbes, G. A., Hijmans, R. J., & Garrett, K. A. (2011). 
 #                 A metamodeling framework for extending the application domain of process-based 
 #                 ecological models. Ecosphere, 2(8), art90. doi:10.1890/ES11-00128.1
-# Licence:      : This program is free software; you can redistribute it and/or modify
-#                 it under the terms of the GNU General Public License as published by
-#                 the Free Software Foundation; either version 2 of the License, or
-#                 (at your option) any later version.
-
-#                 This program is distributed in the hope that it will be useful,
-#                 but WITHOUT ANY WARRANTY; without even the implied warranty of
-#                 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#                 GNU General Public License for more details.
-
-#                 You should have received a copy of the GNU General Public License along
-#                 with this program; if not, write to the Free Software Foundation, Inc.,
-#                 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# Licence:      : GPL3;
 ##############################################################################
 
 ###### Libraries #####
